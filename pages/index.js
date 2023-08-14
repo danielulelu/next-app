64f9892ef0fd78575d60f392cadadb0651539fec
Eleven <danielulelu@gmail.com>
2023-08-14 14:30:24 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import { server } from '../config/index';
+import ArticleList from '../components/ArticleList';
+
+export default function Home({ articles}) {
+  // console.log(articles);
+  return (
+    <div>
+      <ArticleList articles={articles} />
+    </div>
+  )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(`${server}/api/articles`);
+  const articles = await res.json()
+  return {
+    props: {
+      articles
+    }
+  }
+}
